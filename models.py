467185c480a5fbb332ca484bb6aa67c3986c655f
Frederic Beaupre <fred.beaupre32@icloud.com>
2023-03-29 17:08:09 -0400

    feat(files): more files and helper functions

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def FeedForwardNet(torch.nn.Module):
-    def __init__(self, input_dim, hidden_layers, num_layers):
+    def __init__(self, input_dim, hidden_layers, num_classes):
         super().__init__()
         pass
 
