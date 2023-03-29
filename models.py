907cafc90bcbb76a75521ffcf7dab98c0484ee21
Frederic Beaupre <fred.beaupre32@icloud.com>
2023-03-29 16:55:37 -0400

    fix

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def FeedForwardNet(torch.nn.Module):
-    def __init__(self, input_dim, hidden_layers):
-        super()._init__()
+    def __init__(self, input_dim, hidden_layers, num_layers):
+        super().__init__()
         pass
 
     def forward(self, x):
