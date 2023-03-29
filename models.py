37f2ecd67a6b3ccd9e638def128cf07ef84385ac
Frederic Beaupre <fred.beaupre32@icloud.com>
2023-03-29 16:53:31 -0400

    init files

---
+++ models.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+
+def FeedForwardNet(torch.nn.Module):
+    def __init__(self, input_dim, hidden_layers):
+        super()._init__()
+        pass
+
+    def forward(self, x):
+        pass
